9f2e1f3d446e744a9f61cb8fb274f770bef08b6e
sTePanJS396 <84932539+sTePanJS396@users.noreply.github.com>
2021-06-04 21:00:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 
 let money;
-let expenses;
-let sum;
-let y = 0;
 function start() {
     do {
         money = prompt('Какой твой месячный доход?');
@@ -10,8 +7,7 @@ function start() {
 };
 
 start();
-// Спрашиваем у пользователя месячных доход, и записываем его в переменную money
-// Объявим функцию start и реализуем ее функционал с помощью do-while
+
 let appData = {
     income: {},
     addIncome: [],
@@ -29,99 +25,101 @@ let appData = {
         appData.addExpenses = addExpenses.toLocaleLowerCase().split(', ');
         appData.deposit = confirm('У тебя есть депозит в банке? Нажми OK, если есть.');
     },
+
     budget: money,
-    budgetDay: 0,
+    budgetDay: function() {
+        return Math.floor(appData.getBudget() / 30);
+    }
+    ,
     budgetMonth: 0,
-    expensesMonth: y,
+    expensesMonth: 0,
     expMonth: function () {
         let i = 0;
-        while (i < 2) {
-            expenses = prompt('Назови обязательную статью расходов.');
-            sum = +prompt('Во сколько это обойдется?');
-            appData.expenses[expenses] = sum;
-            i++;
+        let expenses;
+        let sum;
+        while (i < 1) {
+            do {
+                expenses = prompt('Назови обязательную статью расходов.');
+                sum = +prompt('Во сколько это обойдется?');
+                appData.expenses[expenses] = sum;
+                i++;
+            } while (isNaN(sum) || sum === '' || sum === null);
         }
         
+    },
+    expensMonth: function getExpensesMonth() {
+        appData.expMonth();
+        let f;
+        for (let key in appData.expenses) {
+            f = appData.expenses[key];
+            
+        }
+        return appData.expensesMonth = f;
+    }, 
+    getBudget: function() {
+        return appData.budget - appData.expensesMonth;
+    },
+
+    getTargetMonth: function() {
+        return Math.round(appData.mission / appData.getBudget());    
+    },
+
+    getStatusIncome: function () {
+        if (appData.budgetDay >= 1200) {
+            return 'У тебя высокий уровень дохода';
+        } else if (600 <= appData.budgetDay <= 1200) {
+            return 'У тебя средний уровень дохода';
+        } else if (0 <= appData.budgetDay <= 600) {
+            return 'К сожалению, уровень дохода у тебя низкий';
+        } else {
+            return 'Что-то пошло не так :(';
+        }
+    },
+
+    getBudgetDay: function() {
+        return Math.floor(appData.getBudget() / 30);
     }
+
 };
 
 appData.asking();
 
-function getExpensesMonth() {
-    appData.expMonth();
-    let f, g;
-    for (let key in appData.expenses) {
-        f = appData.expenses[key];
-        g = appData.expenses[key];
-        
-    }
-    return appData.expensesMonth = f + g;
-}
-getExpensesMonth();
+appData.expensMonth();
 
-
-
-appData.getBudget = function() {
-    return appData.budget - appData.expensesMonth;
-};
 appData.getBudget();
 
-// console.log(appData.getBudget());
-// Объявляем функцию, передаем ей параметры, необходимые для вычесления накоплений за месяц,
-// вызов функции присваеваем в переменную accumulatedMonth.
-// Внесены коррективы: теперь значение вычесляется исходя из переменной sum,
-// полученной в результате выполнения функции getExpensesMonth.
-
-appData.getTargetMonth = function() {
-    return Math.round(appData.mission / appData.getBudget());    
-};
 appData.getTargetMonth(appData.getBudget(), appData.mission);
-// Фунуция getTargetMonth получит данные, и вычислит, за какой период будет достигнута 
-// цель накопить определенную сумму денег
 
-appData.getBudgetDay = function() {
-    return Math.floor(appData.getBudget() / 30);
-};
 appData.budgetDay = appData.getBudget();
-// Высчитываем дневной бюджет
 
-appData.getStatusIncome =  function () {
-    if (appData.budgetDay >= 1200) {
-        return 'У тебя высокий уровень дохода';
-    } else if (600 <= appData.budgetDay <= 1200) {
-        return 'У тебя средний уровень дохода';
-    } else if (0 <= appData.budgetDay <= 600) {
-        return 'К сожалению, уровень дохода у тебя низкий';
-    } else {
-        return 'Что-то пошло не так :(';
-    }
-    // Вычелсяем уровень дохода
-};
 appData.getStatusIncome(appData.budgetDay);
-// Функция высчитывает уровень дохода
 
 
-// ------------------Выводим все необходимые данные в консоль----------------------------
 console.log(appData.expenses);
 
 console.log(appData.getStatusIncome(appData.budgetDay));
-// Уровень дохода
 
 if (appData.getTargetMonth() > 0) {
     console.log('Цель будет достигнута через... ' + appData.getTargetMonth(appData.getBudget(), appData.mission));
 } else {
     console.log('Цель не будет достигнута :(');
 }
-// Добавленна проверка переменной whatGetTargetMonth. 
-// Срок достижения цели накопления 
 
 console.log('Бюджет на день: ', appData.getBudgetDay());
-// Бюджет на день
 
 console.log('Расходы на месяц составили: ' + appData.expensesMonth);
-// Расходы за месяц
-// При вывове функции не передаем значения amount1 and amount2, так как все необходимые
-// данные и вычесление происходят в функции getExpensesMonth.
+
 for (let key in appData) {
     console.log( "Ключ: " + key + " значение: " + appData[key] );
 }
+
+/* 
+Что сделанно:
+1) Удалить все не используемые переменные вне appData - выполнено. ТЗ: "Все остальное почистить в программе у нас всего две переменных money и appData" 
+
+3) Функции getExpensesMonth, getAccumulatedMonth, getTargetMonth, getStatusIncome - сделать методами объекта AppData - ОК, без проблем :)
+
+4) У пользователя нужно спросить дважды, а не четырежды о статье расходов и их стоимости - сделанно!
+
+5) Куда пропала проверка на введенное значение? - Так вот же: (см. Метод exMonth)
+*/
